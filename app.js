35ce79c9552a963990fa4c41ad5e537aaf853248
Boasbabs <babalolasimeon@gmail.com>
2016-06-09 20:22:30 +0530

    add custom service

--- app.js
+++ app.js
@@ -9,12 +9,32 @@ var populationApp = angular.module("populationApp", ["ngRoute", "ngResource"])
 		templateUrl: "main.html",
 		controller: "mainController"
 	})
+	.when("/population", {
+		templateUrl: "population.html",
+		controller: "populationController"
+	})
 })
 	
+//SERVICE
+.service("countryService", function() {
+	this.country = "India";
+	this.year = "2014";
+})
+
 //CONTROLLER
-.controller("mainController", ["$scope", function ($scope) {
+.controller("mainController", ["$scope", "$location", "countryService", function ($scope, $location, countryService) {
 	$scope.country = "india";
 	$scope.year = "2014";
-}]);
 
+	$scope.$watch("country", function () {
+		countryService.city = $scope.country;
+	});
+	$scope.submit = function() {
+		$location.path("/population");
+	};
+}])
+
+.controller("populationController", ["$scope", "$location", "countryService", function ($scope, $location, countryService) {
 	
+	
+}]);
