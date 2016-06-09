731b6f785a73e55bb8a860c0ae321c7a181ce697
Boasbabs <babalolasimeon@gmail.com>
2016-06-09 19:28:02 +0530

    create route and controller

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 //MODULE
 var populationApp = angular.module("populationApp", ["ngRoute", "ngResource"])
 
+ //ROUTES
+.config(function($routeProvider) {
+	$routeProvider
+
+	.when("/", {
+		templateUrl: "main.html",
+		controller: "mainController"
+	})
+})
+	
 //CONTROLLER
+.controller("mainController", ["$scope", function ($scope) {
+	
+}]);
+
+	
